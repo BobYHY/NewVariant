feb889624e22443862a859f36481f474ecef6d3d
Hongyu Yu <109338787+BobYHY@users.noreply.github.com>
2024-06-05 20:15:42 +0800

    Add files via upload

---
+++ algorithm1.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+dis = np.load('Optimal_distance.npy', allow_pickle=True)
+n,_=dis.shape
+dis=dis+np.diag([float("inf")] * n)
+family = np.load('Select_family.npy', allow_pickle=True)
+faindex={"Alpha":[],"Beta":[],"Gamma":[],"Delta":[], "Omicron":[],"Epsilon":[],"Zeta":[],"Eta":[],"Iota":[],"Kappa":[],"Lambda":[],"Mu":[]}
+for i in range(len(family)):
+    faindex[family[i]].append(i)
+
+MaxL=0
+for key in faindex.keys():
+    if MaxL<len(faindex[key]):
+        MaxL=len(faindex[key])
+kkk=int(MaxL/1000)+1
+print(kkk)
+
+def mindis(i, fa):
+    vec = dis[i, faindex[fa]]
+    return np.sort(vec)[kkk-1]  
+
+def distribution(fa1, fa2): 
+    vec=[]
+    for i in faindex[fa2]:
+        vec.append(mindis(i,fa1))
+    return np.array(vec)
+
+def distribution_in():
+    vec=[]
+    for key in faindex.keys():
+        vec.extend(distribution(key,key))
+    return vec
+
+def distribution_out():
+    vec=[]
+    for key1 in faindex.keys():
+        for key2 in faindex.keys():
+            if key1 != key2:
+                vec.extend(distribution(key1,key2))
+    return vec
+
+def nearest_neighbor_classification(dist_matrix, family):
+    ALL=len(family)
+    ACC=0
+    for i in range(ALL):
+        nearest_neighbor_index = np.argmin(dist_matrix[i,:])
+        nearest_neighbor_family = family[nearest_neighbor_index]
+        if nearest_neighbor_family==family[i]:
+            ACC+=1
+    return float(ACC)/float(ALL)
+
+ACC1=0
+ACC2=0
+ALL=0
+vec=distribution_in()
+bound=np.percentile(vec,99)
+for name1 in faindex.keys():
+    acc1=0
+    acc2=0
+    all=len(faindex[name1])
+    for i in faindex[name1]:
+        flag1=0
+        flag2=0
+        for name2 in faindex.keys():
+            if name2==name1:
+                continue
+            elif mindis(i,name2)<bound:
+                flag1=1
+        for name2 in faindex.keys():
+            if mindis(i,name2)<bound:
+                flag2=1
+        if flag1==0:
+            acc1+=1
+        if flag2==1:
+            acc2+=1
+    ACC1+=acc1
+    ACC2+=acc2
+    ALL+=all
+print("{:.2f}".format(100*(1-(ACC1/ALL)))+"\%","{:.2f}".format(100*(1-(ACC2/ALL)))+"\%")
---
+++ metric.py
@@ -0,0 +1,81 @@
+import numpy as np
+from Bio import SeqIO
+
+def Kmer(sequence, K):
+    m=4**K
+    na_vect=[0]*(3*m)
+    pos_sum=[0]*m
+    squa_sum=[0]*m
+    n=len(sequence)-(K-1)
+    index_map = {  'a':0, 'A':0, 'c':1, 'C':1, 'g':2, 'G':2, 't':3, 'T':3  }
+    for i in range(0, n):
+        flag=1
+        for l in range(0,K):
+            if sequence[i+l] not in index_map.keys():
+                flag=0
+        if flag == 0:
+            continue
+        tem=index_map[sequence[i]]
+        for l in range(1,K):
+            tem=4*tem+index_map[sequence[i+l]]
+        na_vect[tem] += 1
+        pos_sum[tem] += i+1
+    for k in range(0,m):
+        if na_vect[k] != 0:
+            na_vect[k+m] = pos_sum[k] / na_vect[k]
+        else:
+            na_vect[k+m]=0
+    for i in range(0, n):
+        flag=1
+        for l in range(0,K):
+            if sequence[i+l] not in index_map.keys():
+                flag=0
+        if flag == 0:
+            continue
+        tem=index_map[sequence[i]]
+        for l in range(1,K):
+            tem=4*tem+index_map[sequence[i+l]]
+        squa_sum[tem] += ( i + 1 - na_vect[tem+m] ) ** 2
+    for k in range(0,m):
+        if na_vect[k] != 0:
+            na_vect[k+2*m] = squa_sum[k] / (n * na_vect[k])
+        else:
+            na_vect[k+2*m]=0
+    return na_vect
+
+def distance(X):
+  n,m = X.shape
+  G = np.dot(X,X.T)
+  H = np.tile(np.diag(G), (n,1))
+  return np.sqrt(H + H.T - 2*G)
+
+seq=[]
+for read in SeqIO.parse("Select.fasta", "fasta"):
+    seq.append((read.seq).__str__())
+N=len(seq)
+
+for K in range(1,10):
+    nv=np.zeros((N,3*(4**K)))
+    for j in range(N):
+        nv[j,:]=Kmer(seq[j],K)
+    for i in range(0,3):
+        nvO=nv[:,i*(4**K):(i+1)*(4**K)]
+        disO=distance(nv)
+        np.save("order_dis\\"+str(K)+"mer_order"+str(i)+"_distance.npy", disO)
+
+dis=[]
+weight=[0.03549459364589514, 0.039082434930630405, 0.30412906453093275, 0.030801860439204753, 0.013913759681552054, 0.0847541874627664, 0.2147908821824287, 0.009746970476623207, 0.037468677500208866, 0.4921144274939822, 0.0017718520847580079, 0.0025613627212840574, 0.7629788655456013, 0.0012773638058877917, 0.005374227624313233, 1.0, 0.0017544541737680417, 0.001553408700637301, 0.8418668476692482, 0.0010680411101253774, 0.00010663595705133611, 0.713477302936144, 0.0010440696319391177, 0.00092914523021887, 0.1843582232771541, 0.000813690120208786, 5.0762169732243335e-06]
+for K in range(1,10):
+    for i in range(0,3):
+        dis.append(np.load("order_dis\\"+str(K)+"mer_order"+str(i)+"_distance.npy", allow_pickle=True))
+disfinal=weight[0]*dis[0]
+for i in range(1,len(weight)):
+    disfinal+=weight[i]*dis[i]
+np.save("Optimal_distance.npy", disfinal)
+    
+
+
+
+
+
+
